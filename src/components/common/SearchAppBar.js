9c9326e54fd9cb3be0e226595f677830ad96d957
RacketyWater7 <haseebudeen@outlook.com>
2022-06-19 12:53:28 +0500

    made a change

--- src/components/common/SearchAppBar.js
+++ src/components/common/SearchAppBar.js
@@ -405,7 +405,7 @@ export default function SearchAppBar() {
             component="div"
             sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}
           >
-            Bethesda
+            Bethessda
           </Typography>
           <Search>
             <SearchIconWrapper>
